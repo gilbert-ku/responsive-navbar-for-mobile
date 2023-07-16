1edafa425db13dc24944cb0b74b529b0fb9d9be9
gilbert-ku <gilbert45ku@gmail.com>
2023-07-16 22:04:51 +0300

    responsive

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
+// const menuToggle = document.querySelector('.menu-toggle');
+// const menu = document.querySelector('.menu');
+
+// menuToggle.addEventListener('click', () => {
+//   menuToggle.classList.toggle('active');
+//   menu.classList.toggle('active');
+// });
+
 const menuToggle = document.querySelector('.menu-toggle');
 const menu = document.querySelector('.menu');
 
 menuToggle.addEventListener('click', () => {
-  menuToggle.classList.toggle('active');
   menu.classList.toggle('active');
+});
+
+// Close the menu when a menu item is clicked
+menu.addEventListener('click', (event) => {
+  if (event.target.tagName === 'A') {
+    menu.classList.remove('active');
+  }
 });
